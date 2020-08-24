14c34fd13e591a32cfcf469b087585d5dbb7c164
Mahammad Aslam <middeaslam36gmail.com>
2020-08-24 19:06:20 +0530

    Secure Storage

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -6,6 +6,7 @@ import About from './AboutComponent';
 import Contact from './ContactComponent';
 import Reservation from './ReservationComponent'
 import Favorites from './FavoriteComponent';
+import Login from './LoginComponent'
 import { View, Platform, Image, StyleSheet, ScrollView, Text } from 'react-native';
 import { createStackNavigator, createDrawerNavigator, DrawerItems, SafeAreaView } from 'react-navigation';
 import { Icon } from 'react-native-elements';
@@ -148,26 +149,59 @@ const FavoritesNavigator = createStackNavigator({
     })
 });
 
+const LoginNavigator = createStackNavigator({
+    Login: { screen: Login }
+}, {
+    navigationOptions: ({ navigation }) => ({
+        headerStyle: {
+            backgroundColor: "#512DA8"
+        },
+        headerTitleStyle: {
+            color: "#fff"
+        },
+        headerTintColor: "#fff",
+        headerLeft: <Icon name="menu" size={24}
+            iconStyle={{ color: 'white' }}
+            onPress={() => navigation.toggleDrawer()} />
+    })
+});
+
 const CustomDrawerContentComponent = (props) => (
     <ScrollView>
         <SafeAreaView style={styles.container}
-            forceInset={{ top: 'always', horizontal: 'never'}}>
-                <View style={styles.drawerHeader}>
-                    <View style={{flex: 1}}>
-                        <Image source={require('./images/logo.png')}
-                            style={styles.drawerImage} />
-                    </View>
-                    <View style={{flex: 2}}>
-                        <Text style={styles.drawerHeaderText}>Ristorante Con Fusion</Text>
-                    </View>
+            forceInset={{ top: 'always', horizontal: 'never' }}>
+            <View style={styles.drawerHeader}>
+                <View style={{ flex: 1 }}>
+                    <Image source={require('./images/logo.png')}
+                        style={styles.drawerImage} />
                 </View>
-                <DrawerItems {...props} />
+                <View style={{ flex: 2 }}>
+                    <Text style={styles.drawerHeaderText}>Ristorante Con Fusion</Text>
+                </View>
+            </View>
+            <DrawerItems {...props} />
         </SafeAreaView>
     </ScrollView>
 );
 
 
 const MainNavigator = createDrawerNavigator({
+
+    Login: {
+        screen: LoginNavigator,
+        navigationOptions: {
+            title: 'Login',
+            drawerLabel: 'Login',
+            drawerIcon: ({ tintColor, focused }) => (
+                <Icon
+                    name='sign-in'
+                    type='font-awesome'
+                    size={24}
+                    iconStyle={{ color: tintColor }}
+                />
+            ),
+        }
+    },
     Home: {
         screen: HomeNavigator,
         navigationOptions: {
@@ -219,11 +253,11 @@ const MainNavigator = createDrawerNavigator({
             title: 'Contact Us',
             drawerLabel: 'Contact Us',
             drawerIcon: ({ tintColor }) => (
-                <Icon 
+                <Icon
                     name='address-card'
                     type='font-awesome'
-                    size = {22}
-                    color = {tintColor}
+                    size={22}
+                    color={tintColor}
                 />
             )
         }
@@ -234,11 +268,11 @@ const MainNavigator = createDrawerNavigator({
             title: 'My Favorites',
             drawerLabel: 'My Favorites',
             drawerIcon: ({ tintColor }) => (
-                <Icon 
+                <Icon
                     name='heart'
                     type='font-awesome'
-                    size = {22}
-                    color = {tintColor}
+                    size={22}
+                    color={tintColor}
                 />
             )
         }
@@ -249,16 +283,17 @@ const MainNavigator = createDrawerNavigator({
             title: 'Reserve Table',
             drawerLabel: 'Reserve TAble',
             drawerIcon: ({ tintColor }) => (
-                <Icon 
+                <Icon
                     name='cutlery'
                     type='font-awesome'
-                    size = {22}
-                    color = {tintColor}
+                    size={22}
+                    color={tintColor}
                 />
             )
         }
     }
 }, {
+    initialRouteName: 'Home',
     drawerBackgroundColor: '#D1C4E9',
     contentComponent: CustomDrawerContentComponent
 })
@@ -283,17 +318,17 @@ class Main extends Component {
 
 const styles = StyleSheet.create({
     container: {
-        flex:1
+        flex: 1
     },
-    drawerHeader:{
+    drawerHeader: {
         backgroundColor: '#512DA8',
-        height:140,
+        height: 140,
         alignItems: 'center',
         justifyContent: 'center',
         flex: 1,
         flexDirection: 'row'
     },
-    drawerHeaderText:{
+    drawerHeaderText: {
         color: 'white',
         fontSize: 24,
         fontWeight: 'bold'
